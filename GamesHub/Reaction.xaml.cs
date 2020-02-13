69dda710d1ca79f06080238bfcdb35cd1c20e950
juztmark <miho.marek3@gmail.com>
2020-02-13 09:21:09 +0100

    Fixed small issues in reaction game

--- GamesHub/Reaction.xaml.cs
+++ GamesHub/Reaction.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,35 +13,42 @@ namespace GamesHub
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Reaction : ContentPage
     {
+        private static Timer timer;
+
         public Reaction()
         {
-            InitializeComponent();
+            InitializeComponent();     
         }
 
         private void StartButton(object sender, EventArgs e)
         {
-            MainButton.Text = null;
-            MainButton.IsEnabled = false; 
-            var wait = new System.Random().Next(3000, 8000);
-            System.Threading.Thread.Sleep(wait);
-            MainButton.BackgroundColor = Color.LimeGreen;
-            
+            MainButton.Text = "";
+            var wait = new Random().Next(3000, 8000);
+            timer = new Timer(wait);
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;               
         }
 
         private void Player1Score(object sender, EventArgs e)
         {
-            MainButton.BackgroundColor = Color.White;
-            DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");
-            MainButton.Text = "START";
-            MainButton.IsEnabled = true;
+            if (MainButton.BackgroundColor == Color.LimeGreen)
+            {
+                MainButton.BackgroundColor = Color.White;
+                DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");                            
+            }
         }
 
         private void Player2Score(object sender, EventArgs e)
         {
-            MainButton.BackgroundColor = Color.White;
-            DisplayAlert("WINNER", "Player2 Won", "JA VIEM", "DAJ MI POKOJ");
-            MainButton.Text = "START";
-            MainButton.IsEnabled = true;
+            if (MainButton.BackgroundColor == Color.LimeGreen)
+            {
+                MainButton.BackgroundColor = Color.White;
+                DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");
+            }
+        }
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {          
+            MainButton.BackgroundColor = Color.LimeGreen;
         }
 
     }
