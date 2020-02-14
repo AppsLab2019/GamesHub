c18e069bbf16b7eda5b3ff2e4819f00453885b4b
juztmark <miho.marek3@gmail.com>
2020-02-14 19:27:42 +0100

    Small code changes

--- GamesHub/Reaction.xaml.cs
+++ GamesHub/Reaction.xaml.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Timers;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,43 +8,39 @@ namespace GamesHub
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Reaction : ContentPage
     {
-        private static Timer timer;
+        private static Timer _timer;
 
         public Reaction()
         {
-            InitializeComponent();     
+            InitializeComponent();
         }
 
         private void StartButton(object sender, EventArgs e)
         {
             MainButton.Text = "";
             var wait = new Random().Next(3000, 8000);
-            timer = new Timer(wait);
-            timer.Elapsed += OnTimedEvent;
-            timer.Enabled = true;               
+            _timer = new Timer(wait);
+            _timer.Elapsed += OnTimedEvent;
+            _timer.Enabled = true;
         }
 
         private void Player1Score(object sender, EventArgs e)
         {
-            if (MainButton.BackgroundColor == Color.LimeGreen)
-            {
-                MainButton.BackgroundColor = Color.White;
-                DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");                            
-            }
+            if (MainButton.BackgroundColor != Color.LimeGreen) return;
+            MainButton.BackgroundColor = Color.White;
+            DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");
         }
 
         private void Player2Score(object sender, EventArgs e)
         {
-            if (MainButton.BackgroundColor == Color.LimeGreen)
-            {
-                MainButton.BackgroundColor = Color.White;
-                DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");
-            }
-        }
-        private void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {          
-            MainButton.BackgroundColor = Color.LimeGreen;
+            if (MainButton.BackgroundColor != Color.LimeGreen) return;
+            MainButton.BackgroundColor = Color.White;
+            DisplayAlert("WINNER", "Player1 Won", "JA VIEM", "DAJ MI POKOJ");
         }
 
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            MainButton.BackgroundColor = Color.LimeGreen;
+        }
     }
 }
