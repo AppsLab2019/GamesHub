a9cba89f7e16519c8327864290c92c85f5bb3f67
Matej Meciak <matej.meciak123@gmail.com>
2020-04-05 15:46:08 +0200

    Code cleanup.

--- GamesHub/ViewModels/Games/TicTacToeViewModel.cs
+++ GamesHub/ViewModels/Games/TicTacToeViewModel.cs
@@ -9,33 +9,25 @@ namespace GamesHub.ViewModels.Games
         private readonly ImageSource _imageCircle = "circle.png";
         private readonly ImageSource _imageCross = "x.png";
 
-        private readonly ImageSource[] _sourceArr;
+        public ImageSource[] SourceArr { get; }
         private int _turn;
 
         public TicTacToeViewModel()
         {
             _turn = 1;
-            _sourceArr = new ImageSource[9];
+            SourceArr = new ImageSource[9];
             ClickCommand = new Command<string>(HandleClick);
         }
 
         public ICommand ClickCommand { get; }
-        public ImageSource FirstSource => _sourceArr[0];
-        public ImageSource SecondSource => _sourceArr[1];
-        public ImageSource ThirdSource => _sourceArr[2];
-        public ImageSource FourthSource => _sourceArr[3];
-        public ImageSource FifthSource => _sourceArr[4];
-        public ImageSource SixthSource => _sourceArr[5];
-        public ImageSource SeventhSource => _sourceArr[6];
-        public ImageSource EighthSource => _sourceArr[7];
-        public ImageSource NinthSource => _sourceArr[8];
+
         public event PropertyChangedEventHandler PropertyChanged;
 
         private void HandleClick(string btnIndex)
         {
             var index = int.Parse(btnIndex);
-            if (_sourceArr[index] != null) return;
-            _sourceArr[index] = _turn++ % 2 == 1 ? _imageCircle : _imageCross;
+            if (SourceArr[index] != null) return;
+            SourceArr[index] = _turn++ % 2 == 1 ? _imageCircle : _imageCross;
 
             HandleWin();
 
@@ -46,24 +38,24 @@ namespace GamesHub.ViewModels.Games
         {
             //rows
             for (var i = 0; i <= 6; i += 3)
-                if (_sourceArr[i] != null && _sourceArr[i] == _sourceArr[i + 1] &&
-                    _sourceArr[i + 1] == _sourceArr[i + 2])
-                    return _sourceArr[i];
+                if (SourceArr[i] != null && SourceArr[i] == SourceArr[i + 1] &&
+                    SourceArr[i + 1] == SourceArr[i + 2])
+                    return SourceArr[i];
             //columns
             for (var j = 0; j <= 2; j++)
-                if (_sourceArr[j] != null && _sourceArr[j] == _sourceArr[j + 3] &&
-                    _sourceArr[j + 3] == _sourceArr[j + 6])
-                    return _sourceArr[j];
+                if (SourceArr[j] != null && SourceArr[j] == SourceArr[j + 3] &&
+                    SourceArr[j + 3] == SourceArr[j + 6])
+                    return SourceArr[j];
 
             //diagonal check; from upper right corner
             const int k = 2;
-            if (_sourceArr[k] != null && _sourceArr[k] == _sourceArr[k + 2] &&
-                _sourceArr[k + 2] == _sourceArr[k + 4]) return _sourceArr[k];
+            if (SourceArr[k] != null && SourceArr[k] == SourceArr[k + 2] &&
+                SourceArr[k + 2] == SourceArr[k + 4]) return SourceArr[k];
 
             //diagonal check; from upper left corner
             const int l = 0;
-            if (_sourceArr[l] != null && _sourceArr[l] == _sourceArr[l + 4] &&
-                _sourceArr[l + 4] == _sourceArr[l + 8]) return _sourceArr[l];
+            if (SourceArr[l] != null && SourceArr[l] == SourceArr[l + 4] &&
+                SourceArr[l + 4] == SourceArr[l + 8]) return SourceArr[l];
 
             return null;
         }
@@ -86,8 +78,8 @@ namespace GamesHub.ViewModels.Games
         private void Reset()
         {
             _turn = 1;
-            for (var i = 0; i < _sourceArr.Length; i++)
-                _sourceArr[i] = null;
+            for (var i = 0; i < SourceArr.Length; i++)
+                SourceArr[i] = null;
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(default));
         }
     }
