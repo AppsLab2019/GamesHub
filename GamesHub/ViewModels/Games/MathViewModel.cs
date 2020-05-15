1cdee07079acc0364ac3ff42dda5d65565369520
juztmark <miho.marek3@gmail.com>
2020-05-15 23:55:38 +0200

    More work done on Math game.

--- GamesHub/ViewModels/Games/MathViewModel.cs
+++ GamesHub/ViewModels/Games/MathViewModel.cs
@@ -12,60 +12,92 @@ namespace GamesHub.ViewModels.Games
         private readonly Random _random = new Random();
         private Operations _operation;
         private int _result;
+        private int _player1Score;
+        private int _player2Score;
 
         public MathViewModel()
         {
-            OperationAnswers = new string[3];
+            OperationAnswers = new int[3];
             AnswerCommand = new Command<string>(HandleClick);
-            GenerateRandomNumbers();
-            GenerateRandomOperation();
-            _operation(_operationNumbers[0], _operationNumbers[1], _operationNumbers[2]);
-            GenerateAnswers();
+            InitializeGame();
         }
 
         public string Operation { get; private set; }
-        public string[] OperationAnswers { get; }
+        public int[] OperationAnswers { get; }
         public ICommand AnswerCommand { get; }
+        public int Player1Score { get; private set; }
+
+        public int Player2Score { get;  private set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        private void HandleClick(string s)
+        private void InitializeGame()
         {
-            if (OperationAnswers[int.Parse(s)] == _result.ToString())
-            {
-            }
+            GenerateMathProblem();
+            GenerateAnswers();
+        }
+        private void HandleClick(string playerAndButtonIndex)
+        {
+            var indexes = playerAndButtonIndex.Split(' ');
+            var player = int.Parse(indexes[0]);
+            var answerIndex = int.Parse(indexes[1]);
+
+            if (player == 1)
+                ManageScore(ref _player1Score, OperationAnswers[answerIndex]);
+            else
+                ManageScore(ref _player2Score, OperationAnswers[answerIndex]);
+            NextRound();
         }
 
-        private void GenerateRandomNumbers()
+        private void ManageScore(ref int score, int answer)
         {
-            for (var i = 0; i < _operationNumbers.Length; i++) _operationNumbers[i] = _random.Next(0, 30);
+            if (answer == _result)
+                score++;
+            else if (score > 0)
+                score--;
+            Player1Score = _player1Score;
+            Player2Score = _player2Score;
         }
-
-        private void GenerateRandomOperation()
+        private void GenerateMathProblem()
         {
             var operations = new Operations[]
                 {Addition, Subtraction, AdditionAndSubtraction, SubtractionAndAddition};
             _operation = operations[_random.Next(0, operations.Length)];
+
+            for (var i = 0; i < _operationNumbers.Length; i++) _operationNumbers[i] = _random.Next(0, 30);
+            _operation(_operationNumbers[0], _operationNumbers[1], _operationNumbers[2]);
         }
 
         private void GenerateAnswers()
         {
-            OperationAnswers[_random.Next(0, OperationAnswers.Length)] = _result.ToString();
+            OperationAnswers[_random.Next(0, OperationAnswers.Length)] = _result;
             for (var i = 0; i < OperationAnswers.Length; i++)
             {
-                if (OperationAnswers[i] == _result.ToString()) continue;
+                if (OperationAnswers[i] == _result) continue;
                 for (var j = 0; j < 99; j++)
                 {
-                    var wrongAnswer = "";
+                    var wrongAnswer = 0;
                     if (_random.Next(0, 1) == 1)
-                        wrongAnswer = _result + _random.Next(0, 5).ToString();
+                        wrongAnswer = _result + _random.Next(0, 5);
                     else
-                        wrongAnswer = (_result - _random.Next(0, 5)).ToString();
+                        wrongAnswer = _result - _random.Next(0, 5);
 
                     if (OperationAnswers.Contains(wrongAnswer)) continue;
                     OperationAnswers[i] = wrongAnswer;
                 }
             }
+        }
+        private void NextRound()
+        {
+            if(_player1Score == 10 || _player2Score == 10)
+                GameEnd();
+            InitializeGame();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(default));
+        }
+
+        private void GameEnd()
+        {
+
         }
 
         private void Addition(int firstNumber, int secondNumber, int thirdNumber)
