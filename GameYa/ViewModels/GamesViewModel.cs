7a8b856ef09910ac2900215bf550e360c38b9fa7
juztmark <miho.marek3@gmail.com>
2020-06-01 11:08:50 +0200

    New icons and game descriptions

--- GameYa/ViewModels/GamesViewModel.cs
+++ GameYa/ViewModels/GamesViewModel.cs
@@ -26,38 +26,38 @@ namespace GameYa.ViewModels
             new Game
             {
                 Name = "Tic-Tac-Toe",
-                Description = "Who's the mastermind? Play with your friend and find out!",
+                Description = "We’ve dug all over, but we can’t find any treasure.",
                 Image = "tictactoe_icon.png"
             },
             new Game
             {
                 Name = "Reaction",
-                Description = "Who's faster? Play with your friend and find out!",
+                Description = "So you are faster than light? Let's prove that!",
                 Image = "reaction_icon.png"
             },
             new Game
             {
                 Name = "Math",
-                Description = "Who's better in math? First to 10 wins!",
+                Description = "Let's see you prove you have a big brain.",
                 Image = "math_icon.png"
             },
             new Game
             {
                 Name = "Color Mind",
-                Description = "Ever wanted to compare your minds? Then come here!",
+                Description = "Be careful! Don't play for too long, because your brain may explode!",
                 Image = "colormind_icon.png"
             },
             new Game
             {
                 Name = "Swipe Game",
-                Description = "All you need to do is keep swiping!",
-                Image = ""
+                Description = "There's only one rule. Just keep swiping!",
+                Image = "swipegame_icon.png"
             },
             new Game
             {
                 Name = "ABC Pattern",
-                Description = "",
-                Image = ""
+                Description = "Yep, another game as simple as this description!",
+                Image = "letterpattern_icon.png"
             }
         };
     }
